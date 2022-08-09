fa4caf9969b66fd44ef466241ee56163f010715b
arlian.gutama <arlian.gutama@gramedia.id>
2022-08-09 12:16:19 +0700

    test-internal

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/arlian/test-internal/internal"
+import (
+	"github.com/arlian/test-internal/internal"
+	"github.com/arlian/test-internal/pkg/product"
+)
 
 func main() {
 	println(internal.New())
+	product.NewProduct()
 }
