7fda6765095a1c090badbc250e946ca220621589
arlian.gutama <arlian.gutama@gramedia.id>
2022-08-09 12:05:00 +0700

    test-internal

--- internal/promo/promo.go
+++ internal/promo/promo.go
@@ -3,3 +3,7 @@ package promo
 func NewPromo() {
 	println("ini internal promo")
 }
+
+func NewPromo2() string {
+	return "internal promo"
+}
---
+++ pkg/product/product.go
@@ -0,0 +1,5 @@
+package product
+
+func NewProduct() {
+	println("this is product package")
+}
